467284b537c187b085ddc149ca49b3ca4b0bd061
thedu <safimuddin2005@gmail.com>
2024-09-02 22:28:24 -0400

    Removed other icons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,6 @@
 import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import navIcon1 from '../assets/img/nav-icon1.svg';
-import navIcon2 from '../assets/img/nav-icon2.svg';
-import navIcon3 from '../assets/img/nav-icon3.svg';
 
 export const NavBar = () => {
   const [activeLink, setActiveLink] = useState('home');
