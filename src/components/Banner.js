60c8adef22233a18dd3609444625abe24baa50bd
thedu <safimuddin2005@gmail.com>
2024-09-02 22:14:57 -0400

    Build finally ran

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback, useMemo } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import headerImg from "../assets/img/header-img.svg";
 import { ArrowRightCircle } from 'react-bootstrap-icons';
@@ -9,26 +9,16 @@ export const Banner = () => {
   const [isDeleting, setIsDeleting] = useState(false);
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
-  const [index, setIndex] = useState(1);
-  const toRotate = ["Web Developer", "Machine Learning Developer", "Math Student"];
+
+  const toRotate = useMemo(() => ["Web Developer", "Machine Learning Developer", "Math Student"], []);
   const period = 2000;
 
-  useEffect(() => {
-    let ticker = setInterval(() => {
-      tick();
-    }, delta);
-
-    return () => { clearInterval(ticker) };
-  }, [delta, text])
-
-  const tick = () => {
+  const tick = useCallback(() => {
     let i = loopNum % toRotate.length;
     let fullText = toRotate[i];
     let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
 
     setText(updatedText);
-
-    console.log(index);
 
     if (isDeleting) {
       setDelta(prevDelta => prevDelta / 2);
@@ -36,33 +26,37 @@ export const Banner = () => {
 
     if (!isDeleting && updatedText === fullText) {
       setIsDeleting(true);
-      setIndex(prevIndex => prevIndex - 1);
       setDelta(period);
     } else if (isDeleting && updatedText === '') {
       setIsDeleting(false);
       setLoopNum(loopNum + 1);
-      setIndex(1);
       setDelta(500);
-    } else {
-      setIndex(prevIndex => prevIndex + 1);
     }
-  }
+  }, [loopNum, toRotate, isDeleting, text, period]);
 
-    return (
-         <section classNmae="banner" id="home">
-            <Container>
-                <Row className="align-items-center">
-                    <Col xs={12} md={6} xl={7}>
-                        <span className="tagline">This is my Portolio</span>
-                        <h1>{'Hi, Im Safi Uddin, a '}<span className="wrap">{text}</span></h1>
-                        <h1>{' @ Georgia Tech'}</h1>
-                        <button onClick={() => console.log('connect')}>Let's connect! <ArrowRightCircle size={25}/></button>
-                    </Col>
-                    <Col xs={12} md={6} xl={5}>
-                        <img src={headerImg} alt="Header Img"/>
-                    </Col>
-                </Row>
-            </Container>
-         </section>
-    )
-}
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => { clearInterval(ticker) };
+  }, [delta, tick]);
+
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="align-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <span className="tagline">This is my Portfolio</span>
+            <h1>{'Hi, I\'m Safi Uddin, a '}<span className="wrap">{text}</span></h1>
+            <h1>{' @ Georgia Tech'}</h1>
+            <button onClick={() => console.log('connect')}>Let's connect! <ArrowRightCircle size={25}/></button>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <img src={headerImg} alt="Header Img"/>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
