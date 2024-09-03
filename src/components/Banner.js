53e381031f00b148375e95e2590fb4c3da040e73
thedu <safimuddin2005@gmail.com>
2024-09-02 21:58:46 -0400

    Made changes to Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,49 +1,52 @@
-import {useState, useEffect} from "react";
-import {Container, Row, Col} from "react-bootstrap";
-import { ArrowRightCircle } from "react-bootstrap-icons";
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
 import headerImg from "../assets/img/header-img.svg";
+import { ArrowRightCircle } from 'react-bootstrap-icons';
 import 'animate.css';
 
 export const Banner = () => {
-    const [loopNum, setLoopNum] = useState(0);
-    const [isDeleting, setIsDeleting] = useState(false); 
-    const toRotate = ["Web Developer", "Machine Learning Developer", "Math Student"];
-    const [text, setText] = useState('');
-    const [delta, setDelta] = useState(300- Math.random() * 100);
-   
-    const period = 800;
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState('');
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [index, setIndex] = useState(1);
+  const toRotate = ["Web Developer", "Machine Learning Developer", "Math Student"];
+  const period = 2000;
 
-    useEffect(() => {
-        let ticker = setInterval(() => {
-            tick();    
-        }, delta);
-    
-        return () => { clearInterval(ticker); }
-    }, [delta, tick, text]);
-    
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
 
-    const tick = () => {
-        let i = loopNum % toRotate.length;
-        let fullText = toRotate[i];
-        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
-        
-        setText(updatedText);
-        if(isDeleting) {
-            setDelta(prevDelta => prevDelta / 2)
-        }
-        if (!isDeleting && updatedText === fullText) {
-            setIsDeleting(true);
-            setIndex(prevIndex => prevIndex - 1);
-            setDelta(period);
-          } else if (isDeleting && updatedText === '') {
-            setIsDeleting(false);
-            setLoopNum(loopNum + 1);
-            setIndex(1);
-            setDelta(500);
-          } else {
-            setIndex(prevIndex => prevIndex + 1);
-          }
+    return () => { clearInterval(ticker) };
+  }, [text])
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    console.log(index);
+
+    if (isDeleting) {
+      setDelta(prevDelta => prevDelta / 2);
     }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setIndex(prevIndex => prevIndex - 1);
+      setDelta(period);
+    } else if (isDeleting && updatedText === '') {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setIndex(1);
+      setDelta(500);
+    } else {
+      setIndex(prevIndex => prevIndex + 1);
+    }
+  }
 
     return (
          <section classNmae="banner" id="home">
